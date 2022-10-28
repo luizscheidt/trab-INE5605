b5bcc4c3600fde169fc2ec9408f366dd3d03d176
Luiz Felipe Eberhardt Buss Scheidt <116039620+luizscheidt@users.noreply.github.com>
2022-10-28 11:20:56 -0300

    Update transferencia.py

--- codigo/modelos/transferencia.py
+++ codigo/modelos/transferencia.py
@@ -1,4 +1,32 @@
+from data import Data
+from conta_corrente import ContaCorrente
+
 class Transferencia:
-    ...
-class Transferencia:
-    ...
+    def __init__(self, origem: ContaCorrente, destino: ContaCorrente, valor: float):
+        self.__valor = valor
+        self.__origem = origem
+        self.__destino = destino
+
+        @property
+        def origem(self):
+            return self.__origem
+
+        @origem.setter
+        def origem(self, origem: ContaCorrente):
+            self.__origem = origem 
+
+        @property
+        def destino(self):
+            return self.__destino
+
+        @destino.setter
+        def destino(self, destino: ContaCorrente):
+            self.__destino = destino 
+
+        @property
+        def valor(self):
+            return self.__valor
+
+        @valor.setter
+        def valor(valor, valor: float):
+            self.__valor = valor
