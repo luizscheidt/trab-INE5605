0fe2ed48ef104fedb64037c310659179a410ae5b
Luiz Felipe Eberhardt Buss Scheidt <116039620+luizscheidt@users.noreply.github.com>
2022-10-28 09:38:41 -0300

    Update saque.py

--- codigo/modelos/saque.py
+++ codigo/modelos/saque.py
@@ -1,2 +1,15 @@
+from data import Data
+from conta_corrente import ContaCorrente
+
 class Saque:
-    ...
+    def __init__(self, valor: float, conta: ContaCorrente):
+        self.__valor = valor
+
+    @property
+    def valor(self):
+        return self.__valor
+
+    @valor.setter
+    def valor(self, valor: float):
+        self.__valor = valor 
+
