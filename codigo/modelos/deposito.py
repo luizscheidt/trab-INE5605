38ab1bdcf56b45e001a811d0b5bba028161c822e
Luiz Felipe Eberhardt Buss Scheidt <116039620+luizscheidt@users.noreply.github.com>
2022-10-28 09:33:33 -0300

    Update deposito.py

--- codigo/modelos/deposito.py
+++ codigo/modelos/deposito.py
@@ -1,2 +1,14 @@
+from data import Data
+from conta_corrente import ContaCorrente
+
 class Deposito:
-    ...
+    def __init__(self, valor: float, conta: ContaCorrente):
+        self.__valor = valor
+
+    @property
+    def valor(self):
+        return self.__valor
+
+    @valor.setter
+    def valor(self, valor: float):
+        self.__valor = valor 
